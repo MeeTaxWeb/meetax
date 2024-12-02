863d422365fcd9e44ddc99c67072545b5cdde462
MeeTaxWeb <nikiandujar13@gmail.com>
2024-12-01 21:20:43 -0400

    Add files via upload

---
+++ assets/js/footer.js
@@ -0,0 +1,97 @@
+class MtFooter extends HTMLElement {
+    constructor() {
+      super();
+      this.innerHTML = `
+
+
+  <!-- Start Footer -->
+    <footer class="bg-dark" id="tempaltemo_footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-success border-bottom pb-3 border-light logo"><img src="./assets/img/logo_meetax_footer_1.png" style="height: 33px;"></h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li>
+                            <i class="fas fa-map-marker-alt fa-fw"></i>
+                            3880 Broadway, New York, NY 10032
+                        </li>
+                        <li>
+                            <i class="fa fa-phone fa-fw"></i>
+                            <a class="text-decoration-none" href="tel:010-020-0340">000-000-0000</a>
+                        </li>
+                        <li>
+                            <i class="fa fa-envelope fa-fw"></i>
+                            <a class="text-decoration-none" href="mailto:admin@meetaxmultiservice.com">admin@meetaxmultiservice.com</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Sercicios</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><a class="text-decoration-none" href="#">Preparación de Impuestos - Income Tax</a></li>
+                        <li><a class="text-decoration-none" href="#">Bookkeeping</a></li>
+                        <li><a class="text-decoration-none" href="#">Remesas</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Más Infor.</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><a class="text-decoration-none" href="#">Inicio</a></li>
+                        <li><a class="text-decoration-none" href="#">Sobre Nosotros</a></li>
+                        <li><a class="text-decoration-none" href="#">Contacto</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="row text-light mb-4">
+                <div class="col-12 mb-3">
+                    <div class="w-100 my-3 border-top border-light"></div>
+                </div>
+                <div class="col-auto me-auto">
+                    <ul class="list-inline text-left footer-icons">
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="http://facebook.com/"><i class="fab fa-facebook-f fa-lg fa-fw"></i></a>
+                        </li>
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="https://www.instagram.com/meetax.multiserviceinc?igsh=MTRmYm1obHhxMnF5bA=="><i class="fab fa-instagram fa-lg fa-fw"></i></a>
+                        </li>
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="https://twitter.com/"><i class="fab fa-twitter fa-lg fa-fw"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- <div class="col-auto">
+                    <label class="sr-only" for="subscribeEmail">Email address</label>
+                    <div class="input-group mb-2">
+                        <input type="text" class="form-control bg-dark border-light" id="subscribeEmail" placeholder="Email address">
+                        <div class="input-group-text btn-success text-light">Subscribe</div>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+
+        <div class="w-100 bg-black py-3">
+            <div class="container">
+                <div class="row pt-2">
+                    <div class="col-12">
+                        <p class="text-left text-light">
+                            Copyright &copy; 2024 MeeTax</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+    <!-- End Footer -->
+
+
+      `;
+    }
+  }
+  
+  customElements.define('mt-footer', MtFooter);
+  
---
+++ assets/js/header.js
@@ -0,0 +1,88 @@
+class MtHeader extends HTMLElement {
+    constructor() {
+      super();
+      this.innerHTML = `
+
+          <!-- Start Top Nav -->
+    <nav class="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top">
+        <div class="container text-light">
+            <div class="w-100 d-flex justify-content-between">
+                <div>
+                    <i class="fa fa-envelope mx-2"></i>
+                    <a class="navbar-sm-brand text-light text-decoration-none" href="mailto:admin@meetaxmultiservise.com">admin@meetaxmultiservise.com</a>
+                    <i class="fa fa-phone mx-2"></i>
+                    <a class="navbar-sm-brand text-light text-decoration-none" href="tel:010-020-0340">000-000-0000</a>
+                </div>
+                <div>
+                    <a class="text-light" href="https://fb.com/templatemo" target="_blank" rel="sponsored"><i class="fab fa-facebook-f fa-sm fa-fw me-2"></i></a>
+                    <a class="text-light" href="https://www.instagram.com/meetax.multiserviceinc?igsh=MTRmYm1obHhxMnF5bA==" target="_blank"><i class="fab fa-instagram fa-sm fa-fw me-2"></i></a>
+                    <a class="text-light" href="https://twitter.com/" target="_blank"><i class="fab fa-twitter fa-sm fa-fw me-2"></i></a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Close Top Nav -->
+
+
+        <!-- Header -->
+        <nav class="navbar navbar-expand-lg navbar-light shadow">
+          <div class="container d-flex justify-content-between align-items-center">
+            <a class="navbar-brand text-success logo h1 align-self-center" href="index.html">
+                <img src="./assets/img/logo_meetax_1_letter_1440x323.png">
+            </a>
+  
+            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" 
+                    data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+  
+            <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" 
+                 id="templatemo_main_nav">
+              <div class="flex-fill">
+                <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
+                  <li class="nav-item">
+                      <a class="nav-link" href="index.html" data-page="index">Inicio</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="services.html" data-page="services">Servicios</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="about.html" data-page="about">Sobre Nosotros</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="contact.html" data-page="contact">Contacto</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <!-- Close Header -->
+
+
+      `;
+      this.resaltarEnlaceActivo();
+    }
+  
+    resaltarEnlaceActivo() {
+      // Obtiene la URL actual
+      const currentPage = window.location.pathname.split("/").pop().replace(".html", "");
+  
+      // Selecciona todos los enlaces del menú
+      const links = this.querySelectorAll('a.nav-link');
+  
+      links.forEach(link => {
+        // Obtiene el valor de data-page del enlace
+        const page = link.getAttribute('data-page');
+  
+        // Si coincide con la página actual, aplica estilos
+        if (page === currentPage || (currentPage === "" && page === "index")) {
+          link.style.color = "#00b63b"; // Cambia a color verde
+        }
+      });
+    }
+  }
+  
+  customElements.define('mt-header', MtHeader);
+  
